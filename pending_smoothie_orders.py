cce35906caae2b941f8754de22795ec78b6e553b
Steven Pomponio <63677862+Pomponst@users.noreply.github.com>
2026-09-20 04:55:07 -0400

    Update pending_smoothie_orders.py
    Modify session to st.connection("snowflake")

--- pending_smoothie_orders.py
+++ pending_smoothie_orders.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.functions import when_matched
 
@@ -10,7 +9,8 @@ st.write(
     """Orders that need to be filled.
     """)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders") \
     .filter(col("ORDER_FILLED") == 0) \
     .collect()
